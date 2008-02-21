16a2f5f45a19705ec9e2be33ecaba58b619f0d64
Dan Weeks <dan@danimal.org>
2008-02-20 21:56:55 -0800

    check the file for existance snag the revision from the repo to a tempfile

--- git-chdiff.py
+++ git-chdiff.py
@@ -11,6 +11,7 @@ import getopt
 import os
 import subprocess
 import sys
+import tempfile
 
 help_message = '''
 git-chdiff <opts> [file1, file2, ...]
@@ -86,6 +87,29 @@ def main(argv=None):
                 # the file is probably not in the git repo
                 print '%s not in git repository.....skipping' % nFile
                 continue
+        except OSError, e:
+            print >>sys.stderr, 'Execution failed:', e
+        # shadow the requested version of the file to a temp file
+        # so we have something to diff against
+        try:
+            p = subprocess.Popen('git show %s:%s' % (revision,nFile), 
+                                 env=os.environ,
+                                 shell=True,
+                                 stdout=subprocess.PIPE, 
+                                 stderr=subprocess.PIPE)
+            p.wait()
+            # git show, it appears, doesn't return a 1 when the
+            # revision/tag isn't valid so we have to scan the output
+            lines = p.stdout.readlines()
+            if lines[0].startswith('fatal:'):
+                print 'problem getting revision of file %s' % nFile
+                print line
+            else:
+                # save the file out
+                tFile = tempfile.mkstemp('.temp', 'git-chdiff', '/var/tmp')
+                if verbose:
+                    print '\ttemp file: %s' % tFile[1]
+                os.fdopen(tFile[0], 'w').write(''.join(lines))
         except OSError, e:
             print >>sys.stderr, 'Execution failed:', e
 
